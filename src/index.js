bc8898ffec0a6d4bc0e3f71207a16cd29e39291e
Roberto VS <rvs.git@gmail.com>
2022-08-07 21:16:26 -0400

    included react, react-dom

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-import "./styles/main.scss"
+
+import { render } from "react-dom";
+
+import "./styles/main.scss";
 
 import cryLaugh from "./images/cryLaugh.png";
 const laughImg = document.getElementById("laughImg");
 laughImg.src = cryLaugh;
+
+// testing if it works
+render(<p>React is connected!</p>, document.getElementById("root"));
